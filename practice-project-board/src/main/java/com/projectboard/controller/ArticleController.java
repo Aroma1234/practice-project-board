61fb1ec1c6be5d7461858d196e70b7dbb3335fab
jiwonjjang <ukkangsolo@gmail.com>
2023-05-11 16:42:36 +0900

    #9 - Spring Security 추가

--- practice-project-board/src/main/java/com/projectboard/controller/ArticleController.java
+++ practice-project-board/src/main/java/com/projectboard/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package com.projectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * /articles
@@ -12,4 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
+    }
 }
