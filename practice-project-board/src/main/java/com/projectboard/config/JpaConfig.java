0ab43d9a2835c8d0d88b1f6358283cc1be9ed81b
jiwonjjang <ukkangsolo@gmail.com>
2023-05-11 11:07:50 +0900

    #1 - Querydsl 디펜던시 실행 및 gitignore 추가

--- practice-project-board/src/main/java/com/projectboard/config/JpaConfig.java
+++ practice-project-board/src/main/java/com/projectboard/config/JpaConfig.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public class JpaConfig {
 
     @Bean
-    public AuditorAware<String> auditorAware() {
+    public AuditorAware<String> auditorAware() {  // jpa auditing을 할 때 마다 사람 이름은 "jiwon"으로 넣게 됨
         return () -> Optional.of("jiwon"); // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될 때, 수정하자
     }
 }
